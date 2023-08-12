2eaa50c6c66eea2211de85dfa079128411bf21e5
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-12 13:52:45 +0530

    WElcome to UC1

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
-namespace LineComparsion
-{
-    internal class LineComparisionDemo {
-        public class Line
-        { 
-            public static void Main(string[] args)
-            {
-                Console.WriteLine("Welcome to line comparasion program");
-                Console.ReadLine();
+using System;
 
-            }
+namespace LineLengthCalculation
+{
+    class Program
+    {
+        static double CalculateLineLength(double x1, double y1, double x2, double y2)
+        {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            double length = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            return length;
         }
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Length Calculation!");
+
+            Console.Write("Enter x-coordinate of the first point: ");
+            double x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter y-coordinate of the first point: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter x-coordinate of the second point: ");
+            double x2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter y-coordinate of the second point: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double lineLength = CalculateLineLength(x1, y1, x2, y2);
+
+            Console.WriteLine($"The length of the line between ({x1}, {y1}) and ({x2}, {y2}) is: {lineLength}");
+        }
     }
-  }
+}
