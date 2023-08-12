0b1719c2cb602503c131a9bb311fbf20b2270950
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-12 13:46:45 +0530

    Welcome to line comparsion program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace LineComparsion
+{
+    internal class LineComparisionDemo {
+        public class Line
+        { 
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to line comparasion program");
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+  }
